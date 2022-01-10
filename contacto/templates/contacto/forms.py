0df56e123095cc064352e57b83efae620487e915
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-01-10 14:32:27 -0300

    forms

--- contacto/templates/contacto/forms.py
+++ contacto/templates/contacto/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class FormularioContacto(forms.Form):
     nombre = forms.CharField(label = 'Nombre', required = True)
     email = forms.CharField(label='Email', required = True)
-    contenido = forms.CharField(label='Contenido')
+    contenido = forms.CharField(label='Contenido', widget= forms.Textarea)
 
 
-    your_name = forms.CharField(label='Your name', max_length=100)
+    # your_name = forms.CharField(label='Your name', max_length=100)
--- contacto/views.py
+++ contacto/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from contacto.templates.contacto import forms
 # from contacto.models import contacto
 
@@ -6,6 +6,16 @@ from contacto.templates.contacto import forms
 
 def contacto(request):
     formulario = forms.FormularioContacto()  
+
+    if request.method == "POST":
+        formulario = forms.FormularioContacto(data=request.POST)
+        if formulario.is_valid():
+            nombre = request.POST.get('nombre')
+            email = request.POST.get('email')
+            contenido = request.POST.get('contenido')
+
+            return redirect("/contacto/?valido")
+
 
     return render(request, "contacto/contacto.html", {'formulario': formulario})
     # ****************** no olvidar registrar la app en el archivo settings **************
